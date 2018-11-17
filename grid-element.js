6718a253297a5c6d75d340599ab200e523e7fd73
Francesco Bedussi <francesco@francescobedussi.it>
2018-11-17 09:00:31 +0100

    fix random error

--- grid-element.js
+++ grid-element.js
@@ -22,16 +22,16 @@ class GridElement extends HTMLElement {
     refreshState() {
         this.state = getNewState();
         this.render();
-        setTimeout(this.refreshState.bind(this), Math.floor(Math.random * 2000));
+        setTimeout(this.refreshState.bind(this), Math.floor(Math.random() * 2000));
     }
 
     render() {
         this.innerHTML = /*html*/`
             <span style="
-                display: inline-block; 
-                height: 30px; 
-                width: ${this.state.width}px; 
-                background-color: ${this.state.color};"
+            display: inline-block; 
+            height: 30px; 
+            width: ${this.state.width}px; 
+            background-color: ${this.state.color};"
             ></span>
         `;
     }
